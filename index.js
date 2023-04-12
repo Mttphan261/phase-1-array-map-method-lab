d47178b110c6028b7ce42834bbb5c463e34d2212
Matt Phan <mttphan261@gmail.com>
2023-04-11 21:33:56 -0500

    completed lab

--- index.js
+++ index.js
@@ -11,6 +11,39 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+function titleCased() {
+  return tutorials.map((word) => {
+    const seperate = word.split(' ');
+    const cap = seperate.map((letter) => letter.charAt(0).toUpperCase() + letter.slice(1))
+    return cap.join(' ');
+  })
 }
+
+console.log(titleCased());
+
+
+// const titleCased = tutorials.map((item) => {
+//   const seperate = item.split(' ');
+//   const cap = seperate.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+//   return cap.join(' ');
+// })
+
+
+
+// const titleCased = () => {
+//   return tutorials
+// }
+
+
+// NOTE: This lab is challenging! You will need to iterate through the tutorials array, modifying the name of each tutorial. To do this, you will also need to access and modify each individual word.
+
+// Some questions to consider:
+
+// How can we "iterate" through individual words in a string?
+// Can we execute an iteration inside an iteration? How?
+// How can we capitalize just the first letter in a word?
+// A couple of hints:
+
+// Break the task into smaller chunks: using the console or a REPL, start by figuring out how to modify one individual element in the tutorials array. Once you've got that working, then figure out how to update the array itself.
+// Use Google!!
